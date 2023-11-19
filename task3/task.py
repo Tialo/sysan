278ea7745ed9c541c7dd78a182c39f912002ea2e
Tialo <tialox@yandex.ru>
2023-11-19 21:59:29 +0300

    task3

---
+++ task3/task.py
@@ -0,0 +1,20 @@
+import csv
+from math import log2
+
+def get_file_data(path):
+    with open(path, newline="") as csvfile:
+        file_data = [[int(s) for s in line] for line in csv.reader(csvfile, delimiter=",", quotechar="|")]
+    return file_data
+
+def get_entropy(data):
+    n = len(data)
+    m = len(data[0])
+    entropy = 0
+    for i in range(n):
+        for j in range(m):
+            if data[i][j]:
+                entropy -= data[i][j] * log2(data[i][j] / (n - 1)) / (n - 1)
+    return entropy
+
+print(get_entropy(get_file_data("task3.csv")))
+
